c16e186da06070fe5f7092d5c4a7a891be90f242
Ruhit Hassan <ruhithassan10@gmail.com>
2024-10-24 15:00:07 +0600

    Credit request list and scss convert

---
+++ src/app/(dashboard)/credit-requests/_components/CreateCreditRequest.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const CreateCreditRequest = () => {
+  return <div>CreateCreditRequest</div>;
+};
+
+export default CreateCreditRequest;
---
+++ src/app/(dashboard)/credit-requests/page.tsx
@@ -0,0 +1,11 @@
+import CreditRequestList from "./_components/CreditRequestList";
+
+const CreditRequests = () => {
+  return (
+    <>
+      <CreditRequestList />
+    </>
+  );
+};
+
+export default CreditRequests;
